34e99a746015de59bac8fbec6ee5a6f29bab2593
walid hasan <walid.official8@gmail.com>
2024-09-22 00:15:17 +0600

    maximum tasks are added

--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@ let apply_input = document.getElementById('applyInput');
 let apply_btn = document.getElementById('applyBtn');
 let grandTotal = document.getElementById('grand-total');
 let totalPrice = document.getElementById('total-price');
-let selectedSeat =   document.getElementById('selected-option');
+let selectedSeat = document.getElementById('selected-option');
+const nextBtn = document.getElementById('next-btn');
+const nameInput = document.getElementById('name-input')
+const emailInput = document.getElementById('email-input')
+const phoneInput = document.getElementById('phone-input')
 let sum = 0;
-let counting = 0;
+
 
 for(const seat of seatBook){
    
@@ -29,8 +33,8 @@ for(const seat of seatBook){
     let mainPriceNumber = Number(mainPrice.innerText);
     sum += mainPriceNumber
     resultTotalPrice = sum;
-
     totalPrice.innerText = resultTotalPrice.toFixed(2);
+
     // Step-3
     let seatNumber = document.getElementById('seat-number')
     seatNumberConvert = Number(seatNumber.innerText);
@@ -44,13 +48,6 @@ for(const seat of seatBook){
     }
     })
 
-    seat.addEventListener("click", function(){
-        counting++;
-        if(counting == 2){
-            alert('This button is taken');
-            counting = 0;
-        }
-    })
 }
 
 apply_btn.addEventListener('click', function(){
@@ -60,3 +57,88 @@ apply_btn.addEventListener('click', function(){
      grandTotal.innerText = resultGrand.toFixed(2);
     }
 })
+
+
+
+
+
+phoneInput.addEventListener('keyup', function(event){
+    if(event.target.value.length !== 11){
+        nextBtn.setAttribute("disabled", true);
+    }else{
+        nextBtn.removeAttribute("disabled");
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let optionContainer = document.getElementsByClassName('ticketValue');
+// //    console.log(optionContainer);
+//    let optionArray = []
+//    for(let i =0; i < optionContainer.length; i++){
+//     optionArray.push(optionContainer[i].innerText);
+//    }
+//    console.log(optionArray);
+//     if(selectedSeat.childElementCount < 4 && !optionArray.includes(event.target.innerText)){
+//         selectedSeat.innerHTML += option;
+//     }else{
+//         alert('Maximum seat is selected')
+//     }
